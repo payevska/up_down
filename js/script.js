af9fdd85ca35884a98edd723cf58c86bca66aed5
Yuliya Payevska <y.payevska@gmail.com>
2019-11-09 20:40:42 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+   /* // byrger 
+
+    
+    (function($){
+        $(function() {
+            $('.byrger-menu__byrger-icon').on('click', function() {
+                $(this).closest('.byrger-menu').toggleClass('byrger-menu_state_open');
+            });
+        });
+    })(jQuery);
+
+    // for Read more in Blog page
+
+    $('.block-blog__info-blog_link1-blog').click(function () {
+    $('#span1').show();
+
+    });
+
+    $('.block-blog__info-blog_link2-blog').click(function () {
+    $('#span2').show();
+
+    });
+
+    $('.block-blog__info-blog_link3-blog').click(function () {
+    $('#span3').show();
+    
+    });*/
+
+
+
+});
+
+
+
