4474c880d2b1ae40c955886e6547a1b7e2c69188
Yuliya Payevska <y.payevska@gmail.com>
2019-11-25 23:13:25 +0200

    doing slide in portfolio-page

--- js/script.js
+++ js/script.js
@@ -28,9 +28,62 @@ $(document).ready(function(){
     
     });*/
 
+/*scroll-home......https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2*/
 
+$('a').on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+
+
+
+/*carousel....  https://www.youtube.com/watch?v=casgv9Y8I1w&t=28s*/
+
+document.getElementById('slider-top').onclick = sliderTop;
+var top = 0;
+
+function sliderTop(){
+    var polosa = document.getElementById('polosa');
+    top = top - 130;
+    if(top < -520) {
+        top = -520;
+    }
+    polosa.style.top = top + 'px';
+}
+
+document.getElementById('slider-bottom').onclick = sliderBottom;
+
+
+function sliderBottom(){
+    var polosa = document.getElementById('polosa');
+    top = top + 130;
+    if(top > 0) {
+        top = 0;
+    }
+    polosa.style.top = top + 'px';
+}
+
+/*function myFunction() {
+  document.getElementById('demo').innerHTML = document.getElementById('imgs').img;
+}*/
 
 });
-
-
 
