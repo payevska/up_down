28d730452ecf10565c2ab45fbf1339492d501dd8
Yuliya Payevska <y.payevska@gmail.com>
2019-12-11 19:32:45 +0200

    fixing 2 vidio-review for ipad

--- js/script.js
+++ js/script.js
@@ -28,61 +28,61 @@ $(document).ready(function(){
     
     });*/
 
-/*scroll-home......https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2*/
+    /*scroll-home......https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2*/
 
-$('a').on('click', function(event) {
+    $('a').on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+          // Store hash
+          var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
       });
-    } // End if
-  });
 
 
 
 
-/*carousel....  https://www.youtube.com/watch?v=casgv9Y8I1w&t=28s*/
+    /*carousel....  https://www.youtube.com/watch?v=casgv9Y8I1w&t=28s*/
 
-document.getElementById('slider-top').onclick = sliderTop;
-var top = 0;
+    document.getElementById('slider-top').onclick = sliderTop;
+    var top = 0;
 
-function sliderTop(){
-    var polosa = document.getElementById('polosa');
-    top = top - 90;
-    if(top < -360) {
-        top = -360;
+    function sliderTop(){
+        var polosa = document.getElementById('polosa');
+        top = top - 90;
+        if(top < -360) {
+            top = -360;
+        }
+        polosa.style.top = top + 'px';
     }
-    polosa.style.top = top + 'px';
-}
 
-document.getElementById('slider-bottom').onclick = sliderBottom;
+    document.getElementById('slider-bottom').onclick = sliderBottom;
 
 
-function sliderBottom(){
-    var polosa = document.getElementById('polosa');
-    top = top + 90;
-    if(top > 0) {
-        top = 0;
+    function sliderBottom(){
+        var polosa = document.getElementById('polosa');
+        top = top + 90;
+        if(top > 0) {
+            top = 0;
+        }
+        polosa.style.top = top + 'px';
     }
-    polosa.style.top = top + 'px';
-}
 
-/*image portfolio-left.... http://www.web.cofp.ru/vse-o-sajtakh/
-sozdanie-sajta/javascript/jquery/154-uvelichenie-izobrazheniya-pri-nazhatii-na-nego*/ 
+    /*image portfolio-left.... http://www.web.cofp.ru/vse-o-sajtakh/
+    sozdanie-sajta/javascript/jquery/154-uvelichenie-izobrazheniya-pri-nazhatii-na-nego*/ 
 
     $(".carousel-foto__project_fotoProject").click(function(){   // Событие клика на маленькое изображение
         var img = $(this);  // Получаем изображение, на которое кликнули
@@ -99,10 +99,10 @@ sozdanie-sajta/javascript/jquery/154-uvelichenie-izobrazheniya-pri-nazhatii-na-n
     });
 
 
-/* for active a ... http://jsfiddle.net/sdLs7urp/1/
-  https://toster.ru/q/136525*/ 
+    /* for active a ... http://jsfiddle.net/sdLs7urp/1/
+      https://toster.ru/q/136525*/ 
 
-var links = $('.link-active');
+    var links = $('.link-active');
     
     links.first().addClass('active');
     
@@ -111,13 +111,18 @@ var links = $('.link-active');
         $(this).addClass('active');
     });
 
+
+    /*instead vh ----   https://css-tricks.com/the-trick-to-viewport-units-on-mobile/*/
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+
+    
+
 });
 
-/*instead vh ----   https://css-tricks.com/the-trick-to-viewport-units-on-mobile/*/
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
 
