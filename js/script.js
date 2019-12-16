8aca0ded3a50dadac3d20f89794fc50110513ab5
Yuliya Payevska <y.payevska@gmail.com>
2019-12-16 23:15:45 +0200

    did gambyrger for menu

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 
    // byrger 
 
-    
+    /*
     (function($){
         $(function() {
             $('.byrger-menu__byrger-icon').on('click', function() {
                 $(this).closest('.byrger-menu').toggleClass('byrger-menu_state_open');
             });
         });
-    })(jQuery);
+    })(jQuery);*/
 
     // for Read more in Blog page
 
