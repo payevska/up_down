2152b4f369c19c7c85a7d229437fdc1cb6a09247
Yuliya Payevska <y.payevska@gmail.com>
2020-01-04 18:36:35 +0200

    change logotype-img

--- js/script.js
+++ js/script.js
@@ -86,15 +86,8 @@ $(document).ready(function(){
     });
 
 
-   /* $('.gambyrger-menu__toggler').on('click', function() {
-      document.getElementById("gambyrger-close").style.visibility = "visible";
-    });
-  
-    $('.gambyrger-link').on('click', function() {
-      document.getElementById("gambyrger-close").style.visibility = "hidden";
-      document.getElementById("gambyrger-close").style.visibility = "visible";
-    });*/
-    
+   
+    // burger-menu in header-page
     function burgerMenu(selector) {
         let menu = $(selector);
         let button = menu.find('.burger-menu__burger-button');
@@ -133,31 +126,3 @@ $(document).ready(function(){
 
 
 
-
-/*function burgerMenu(selector) {
-        let menu = $(selector);
-        let button = menu.find('.burger-menu__button');
-        let links = menu.find('.burger-menu__link');
-        let overlay = menu.find('.burger-menu__overlay');
-
-        button.on('click', (e) => {
-            e.preventDefault();
-            toggleMenu();
-        });
-
-        links.on('click', () => toggleMenu());
-        overlay.on('click', () => toggleMenu());
-
-        function toggleMenu() {
-            menu.toggleClass('burger-menu__active');
-
-            if  (menu.hasClass('burger-menu__active')) {
-                $('body').css('overflow', 'hidden');
-            } else {
-                $('body').css('overflow', 'visible');
-            }
-        }
-
-    }
-  
-    burgerMenu('.burger-menu');*/
